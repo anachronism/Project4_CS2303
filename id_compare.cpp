d7ae73615a7cc907d881f6df21d3714f6932c5a7
Max Li <maxhongmingli@gmail.com>
2014-09-28 14:34:23 -0400

    Created id_compare, which is unfinished

---
+++ id_compare.cpp
@@ -0,0 +1,9 @@
+#include "global.h"
+//Function written by Max Li
+
+//Compares the id's of two items.  
+int id_compare(ItemPtr currentNode, ItemPtr newNode){
+  string current = currentNode->name;
+  string new = newNode->name;
+
+}
