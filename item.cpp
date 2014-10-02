8fdf2b665db117cbf30e42339e17332e9957835d
Max Li <maxhongmingli@gmail.com>
2014-10-02 12:37:37 -0400

    fixed id_compare logic

--- item.cpp
+++ item.cpp
@@ -23,20 +23,20 @@ Item::Item(string newName){
 }
 
 //Compares the member item to a new item.
-//returns -1 if member item is earlier than new item, 0 if they are the same
-// and 1 if member item is later than new item.
+//returns -1 new item is earlier than the current item, 0 if they are the same
+// and 1 if new item is later than current item.
 int Item::id_compare(ItemPtr newItem){
   string current = getName();
   string new_id = newItem->getName();
 
   if ( current.compare(new_id) < 0){  //if new is lower than current
-    return -1;
+    return 1;
   }
   else if(current.compare(new_id) == 0){ // if new is the same as current
     return 0;
   }
   else {  //if new is higher than current
-    return 1;
+    return -1;
   }
 
 }
